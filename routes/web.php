2baa9f8adc25e482c8dc9327f1030f7fcfd798c6
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2023-01-07 14:47:36 +0700

    feat/install package

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,12 @@ Route::get('/register', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
